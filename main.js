82f0030412d6c8bef8b7f6a14e9446d8a1027d72
Ibrohim0056 <ibrohimjonathamjonov@gmail.com>
2024-12-02 14:58:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+ let body = document.querySelector("body")
+
+ let text = document.querySelector(".text2")
+
+ text.addEventListener("click", function(){
+    body.classList.toggle('white')
+ })
+
+
+
+
+
